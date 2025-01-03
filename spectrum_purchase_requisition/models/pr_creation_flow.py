54da94758d5aa43ba928ea896d21d87cad8d31fd
shahid139 <shahid.3j@gmail.com>
2025-01-03 12:51:45 +0530

    Skip PO level for Petty cash

--- spectrum_purchase_requisition/models/pr_creation_flow.py
+++ spectrum_purchase_requisition/models/pr_creation_flow.py
@@ -30,6 +30,129 @@ class PurchaseRequisitionCreation(models.Model):
     pr_type_check = fields.Boolean()
     is_po_need = fields.Boolean()
 
+    def action_create_invoice(self):
+
+        """Create the invoice associated to the PO.
+        """
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+
+        # 1) Prepare invoice vals and clean-up the section lines
+        invoice_vals_list = []
+        sequence = 10
+        for order in self:
+            if order.state == 'open' and order.is_po_need:
+                continue
+
+            order = order.with_company(order.company_id)
+            pending_section = None
+            # Invoice values.
+            invoice_vals = order._prepare_invoice()
+            # Invoice line values (keep only necessary sections).
+            for line in order.order_line:
+                if line.display_type == 'line_section':
+                    pending_section = line
+                    continue
+                if not float_is_zero(line.qty_to_invoice, precision_digits=precision):
+                    if pending_section:
+                        line_vals = pending_section._prepare_account_move_line()
+                        line_vals.update({'sequence': sequence})
+                        invoice_vals['invoice_line_ids'].append((0, 0, line_vals))
+                        sequence += 1
+                        pending_section = None
+                    line_vals = line._prepare_account_move_line()
+                    line_vals.update({'sequence': sequence})
+                    invoice_vals['invoice_line_ids'].append((0, 0, line_vals))
+                    sequence += 1
+            invoice_vals_list.append(invoice_vals)
+
+        if not invoice_vals_list:
+            raise UserError(_('There is no invoiceable line. If a product has a control policy based on received quantity, please make sure that a quantity has been received.'))
+
+        # 2) group by (company_id, partner_id, currency_id) for batch creation
+        new_invoice_vals_list = []
+        for grouping_keys, invoices in groupby(invoice_vals_list, key=lambda x: (x.get('company_id'), x.get('partner_id'), x.get('currency_id'))):
+            origins = set()
+            payment_refs = set()
+            refs = set()
+            ref_invoice_vals = None
+            for invoice_vals in invoices:
+                if not ref_invoice_vals:
+                    ref_invoice_vals = invoice_vals
+                else:
+                    ref_invoice_vals['invoice_line_ids'] += invoice_vals['invoice_line_ids']
+                origins.add(invoice_vals['invoice_origin'])
+                payment_refs.add(invoice_vals['payment_reference'])
+                refs.add(invoice_vals['ref'])
+            ref_invoice_vals.update({
+                'ref': ', '.join(refs)[:2000],
+                'invoice_origin': ', '.join(origins),
+                'payment_reference': len(payment_refs) == 1 and payment_refs.pop() or False,
+            })
+            new_invoice_vals_list.append(ref_invoice_vals)
+        invoice_vals_list = new_invoice_vals_list
+
+        # 3) Create invoices.
+        moves = self.env['account.move']
+        AccountMove = self.env['account.move'].with_context(default_move_type='in_invoice')
+        for vals in invoice_vals_list:
+            moves |= AccountMove.with_company(vals['company_id']).create(vals)
+
+        # 4) Some moves might actually be refunds: convert them if the total amount is negative
+        # We do this after the moves have been created since we need taxes, etc. to know if the total
+        # is actually negative or not
+        moves.filtered(lambda m: m.currency_id.round(m.amount_total) < 0).action_switch_move_type()
+
+        return self.action_view_invoice(moves)
+
+    def check_budget(self):
+        self.ensure_one()
+        if not self.line_ids:
+            raise UserError(_("You cannot confirm agreement '%s' because there is no product line.", self.name))
+        if self.budget_task:
+            planned_amount = sum([v.planned_amount for v in self.budget_task.crossovered_budget_line])
+            practical_amount = sum([v.practical_amount for v in self.budget_task.crossovered_budget_line])
+            requisition_amount = sum([v.total for v in self.line_ids])
+            available_amount = planned_amount - practical_amount
+            if requisition_amount > available_amount:
+                self.state = 'cancel'
+                sticky_notify = {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': _('Validation'),
+                        'message': 'No Funds Available for PR creation.',
+                        'sticky': False,
+                        'next': {
+                            'type': 'ir.actions.act_window_close'
+                        },
+                    }
+                }
+            else:
+                sticky_notify = {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': _('Validation'),
+                        'message': 'Funds Available for PR creation.',
+                        'sticky': False,
+                        'next': {
+                            'type': 'ir.actions.act_window_close'
+                        },
+                    }
+                }
+
+            return sticky_notify
+    @api.model_create_multi
+    def create(self, vals_list):
+        # Ensure `company_id` is available in the input values
+        for vals in vals_list:
+            # Retrieve the company_id from the vals or set a default
+            company_id = vals.get('company_id', self.env.company.id)
+            # Get the sequence value
+            vals['name'] = self.env['ir.sequence'].with_company(company_id).next_by_code('purchase.requisition.code')
+        # Call the super method with updated vals_list
+        return super(PurchaseRequisitionCreation, self).create(vals_list)
+
     @api.onchange('currency_id')
     def validate_currency(self):
         if self.currency_id:
@@ -57,6 +180,9 @@ class PurchaseRequisitionCreation(models.Model):
             requisition.state_blanket_order = requisition.state
 
     def first_approval(self):
+        self.ensure_one()
+        if not self.line_ids:
+            raise UserError(_("You cannot confirm agreement '%s' because there is no product line.", self.name))
         if self.budget_task:
             planned_amount = sum([v.planned_amount for v in self.budget_task.crossovered_budget_line])
             practical_amount = sum([v.practical_amount for v in self.budget_task.crossovered_budget_line])
@@ -76,6 +202,7 @@ class PurchaseRequisitionCreation(models.Model):
                         },
                     }
                 }
+
                 return sticky_notify
         self.write({
             'state_blanket_order': 'first_approval',
@@ -90,10 +217,6 @@ class PurchaseRequisitionCreation(models.Model):
 
 
     def action_in_progress(self):
-        self.ensure_one()
-        if not self.line_ids:
-            raise UserError(_("You cannot confirm agreement '%s' because there is no product line.", self.name))
-
         if self.type_id.quantity_copy == 'none' and self.vendor_id:
             for requisition_line in self.line_ids:
                 if requisition_line.price_unit <= 0.0:
@@ -104,9 +227,7 @@ class PurchaseRequisitionCreation(models.Model):
             self.write({'state': 'ongoing'})
         else:
             self.write({'state': 'in_progress'})
-        # Set the sequence number regarding the requisition type
-        if self.name == 'New':
-            self.name = self.env['ir.sequence'].with_company(self.company_id).next_by_code('purchase.requisition.code')
+
 
 class PurchaseRequisitionLineInherited(models.Model):
     _inherit = "purchase.requisition.line"
