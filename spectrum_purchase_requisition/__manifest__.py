8a689d537ef410398a308eac6dcfac4e558fe540
shahid139 <shahid.3j@gmail.com>
2025-01-12 09:47:11 +0530

    Company XML Code Commented.

--- spectrum_purchase_requisition/__manifest__.py
+++ spectrum_purchase_requisition/__manifest__.py
@@ -18,7 +18,7 @@
         'views/pr_creation_flow.xml',
         'views/po_creation_flow.xml',
         'views/account_move.xml',
-        'views/res_company.xml',
+        # 'views/res_company.xml',
         'reports/purchase_order_report.xml',
         'reports/rfq_custom.xml',
 
